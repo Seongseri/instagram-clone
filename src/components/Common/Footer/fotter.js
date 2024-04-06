5ae2a30137d1f2f5bc8a1b9295b9c6ee9ad85f55
Seongseri <tjdtpfl@gmail.com>
2024-04-06 16:14:13 +0900

    feat: 로그인 UI 구현

---
+++ src/components/Common/Footer/fotter.js
@@ -0,0 +1,30 @@
+<footer>
+              <div className="container">
+                <nav className="footer-nav">
+                  <ul>
+                    <li>
+                      <a href="#">About Us</a>
+                    </li>
+                    <li>
+                      <a href="#">Support</a>
+                    </li>
+                    <li>
+                      <a href="#">Jobs</a>
+                    </li>
+                    <li>
+                      <a href="#">Privacy</a>
+                    </li>
+                    <li>
+                      <a href="#">Terms</a>
+                    </li>
+                    <li>
+                      <a href="#">Profiles</a>
+                    </li>
+                    <li>
+                      <a href="#">Languages</a>
+                    </li>
+                  </ul>
+                </nav>
+                <div className="copyright-notice">&copy; 2019 Complaints</div>
+              </div>
+            </footer>
